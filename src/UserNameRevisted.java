52f85aa7e3dbaca56b474dadf54b8fe50d08f7d7
mcheich <mcheich@gmail.com>
2020-01-08 09:51:45 -0500

    First Commit

---
+++ src/UserNameRevisted.java
@@ -0,0 +1,24 @@
+
+public class UserNameRevisted {
+
+	public static void main(String[] args) {
+		/* 	User Name revisited
+			Ask the user for a user name of at least 4 characters in length. 
+			Continue prompting the user until they actually enter a user name that is long enough.
+
+			Examples
+			Enter a user name:
+			bob
+
+			not long enough! try again!
+
+			bob
+			still not long enough! try again!!!!
+			
+			bobby
+			Welcome bobby!
+		*/
+
+	}
+
+}
